b72d3ccb969d6a0aa84d222593d4ddb26b9a479f
Danish <dkj23@uclive.ac.nz>
2022-04-27 15:07:58 +1200

    Finished the main function of the program and started manual testing #pair[dkj23, lnb28]

--- routing.py
+++ routing.py
@@ -1,5 +1,6 @@
 """Implementing a RIP routing protocol"""
 
+import random
 import socket
 import json
 import select
@@ -7,13 +8,22 @@ import sys
 
 inputPorts = []
 outputPorts = []
-router_id = 1
 INFINITY = 16
+TIMEOUT = 180
+GARBAGE_COLLECTION = 1
+GARBAGE_TIMER = 120
 
 
 def open_file():
     """Reads the content of the file and formats it into a table"""
-    with open('config_file_1') as f:
+    arguments = sys.argv
+    filename = arguments[1]
+    if len(arguments) != 2:
+        print("Invalid number of arguments.\n Please enter in format: python3 routing.py config_file_(number)")
+        exit()
+    global router_id
+    router_id = int(filename[12])
+    with open(filename) as f:
         contents = f.readlines()
         routerIdRaw = contents[0]
         inputPortsRaw = contents[1]
@@ -65,7 +75,7 @@ def open_file():
 
 
 def create_packet(table):
-    """The header of the RIP packet"""
+    """The header and packet of the RIP packet"""
     command = 2
     version = 2
     zeroField = router_id
@@ -81,10 +91,9 @@ def create_packet(table):
 def check_packet_header(packet):
     """Check the packet header format is as it should be"""
     checkHeader = True
-    header = packet['header']
-    if int(header[0]) != 2 or int(header[1]) != 2:  # Check command and version are 2
+    if int(packet['header'][0]) != 2 or int(packet['header'][1]) != 2:  # Check command and version are 2
         checkHeader = False
-    elif int(header[2]) < 1 or int(header[2]) > 64000:  # Check header port is within range
+    elif int(packet['header'][2]) < 1 or int(packet['header'][2]) > 64000:  # Check header port is within range
         checkHeader = False
     return checkHeader
 
@@ -108,13 +117,15 @@ def open_socket(input_ports):
         try:
             sock.bind(("127.0.0.1", int(inputSocket)))
             socket_table.append(sock)
-        except OSError:
+        except OSError as e:
+            print(e)
             print("Socket bind unsuccessful\n")
             exit()
     return socket_table
 
 
 def close_socket(socket_table):
+    """Closes the socket for every port"""
     try:
         for entry in socket_table:
             entry.close()
@@ -166,18 +177,8 @@ def send_packet(table):
     """Sending the UDP datagrams to neighbours"""
     for outputPort in outputPorts:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(json.dumps(create_packet(table).encode('ascii')), ("127.0.0.1", outputPort))
+        sock.sendto(json.dumps(create_packet(table)).encode('ascii'), ("127.0.0.1", int(outputPort)))
     print("Packet sent successfully!")
-
-
-def print_routing_table(table):
-    """Prints routing table in pretty format"""
-    print("--" * 40)
-    print("Routing table for router {}".format(router_id))
-    for key, data in table.items():
-        print("Destination: {}  Metric: {}  Next Hop: {}  Timer: {}  Garbage Timer: {}".format(key, data[0], data[1],
-                                                                                               data[3], data[4]))
-    print("--" * 40)
 
 
 def response_messages(sockets, timeout, table):
@@ -217,12 +218,74 @@ def response_messages(sockets, timeout, table):
     return packet_table
 
 
+# def update_timer_table(main_table, timer, random_timer):
+#     """Updates the table and the timer and sends the packets when the required conditions are met"""
+#     # if timer == random_timer: # Waits for a random interval of 30+/-5 seconds and then sends a packet while resetting the timer
+#     #     send_packet(main_table)
+#     #     timer = 0
+#     # for id in main_table.keys():
+#     #     if main_table[id][3] >= TIMEOUT: # Checks if the timer exceeds the timeout value
+#     #         main_table[id][0] = INFINITY # Sets metric to infinity
+#     #         main_table[id][2] = True # Sets flag to true
+#     #     else:
+#     #         main_table[id][3] += 1 # Timer goes up by 1
+#     #
+#     #     if main_table[id][2]: # Checks if the flag is true, and if yes increments the garbage collection
+#     #         main_table[id][4] += GARBAGE_COLLECTION
+#     #         if main_table[id][4] >= GARBAGE_TIMER:
+#     #             del main_table[id]
+
+
+def print_routing_table(table):
+    """Prints routing table in pretty format"""
+    print("--" * 40)
+    print("Routing table for router {}".format(router_id))
+    for key, data in table.items():
+        print(
+            "Destination: {}  Metric: {}  Next Hop: {}  Timer: {}  Garbage Collection: {}".format(key, data[0], data[1],
+                                                                                                  data[3], data[4]))
+    print("--" * 40)
+
 
 def main():
-    test = open_file()
-    print(create_packet(test))
-    print(test)
-    print_routing_table(test)
+    """The main function of the file which runs the program"""
+    # test = open_file()
+    # print(create_packet(test))
+    # print(test)
+    # print_routing_table(test)
+    timer = 0
+    random_timer = random.randint(3, 9)  # Offsetting the 30-second timer by a random interval (30 +- 5)
+    # Parses through the config file and formats the data into a table
+    main_table = open_file()
+
+    # Open the sockets for input ports and gets them ready for receiving packets
+    sockets = open_socket(inputPorts)
+
+    while True:
+        timer = timer + 1
+        print_routing_table(main_table)
+
+        # update_timer_table(main_table, timer, random_timer)
+        if timer == random_timer:  # Waits for a random interval of 30+/-5 seconds and then sends a packet while resetting the timer
+            send_packet(main_table)
+            timer = 0
+
+        for id in sorted(main_table.keys()):
+
+            if main_table[id][3] >= TIMEOUT:  # Checks if the timer exceeds the timeout value
+                main_table[id][0] = INFINITY  # Sets metric to infinity
+                main_table[id][2] = True  # Sets flag to true
+            else:
+                main_table[id][3] += 1  # Timer goes up by 1
+
+            if main_table[id][2]:  # Checks if the flag is true, and if yes increments the garbage collection
+                main_table[id][4] += GARBAGE_COLLECTION
+                if main_table[id][4] >= GARBAGE_TIMER:
+                    del main_table[id]
+
+        main_table = response_messages(sockets, 1, main_table)
+
+    close_socket(main_table)
 
 
 main()
